7d5e816da9bb48098a91cb7bac9ff7a37f8e477f
ISC-EIbarra <EdsonP4100@gmail.com>
2024-06-10 20:19:21 -0700

    Fix
    Update Task Controller

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -87,4 +87,11 @@ router.put(
   TaskController.updateTask
 );
 
+router.delete(
+  '/:projectId/tasks/:taskId',
+  param('taskId').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  TaskController.deleteTask
+);
+
 export default router;
