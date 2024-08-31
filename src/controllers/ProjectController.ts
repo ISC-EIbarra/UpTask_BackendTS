7a3401a6c91660b15ab0ba759a25fd98adfbb438
ISC-EIbarra <EdsonP4100@gmail.com>
2024-08-31 15:07:55 -0700

    User verification

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -17,7 +17,7 @@ export class ProjectController {
   static getAllProjects = async (req: Request, res: Response) => {
     try {
       const projects = await Project.find({
-        $or: [{ manager: { $in: req.user.id } }],
+        $or: [{ manager: { $in: req.user._id } }],
       });
       res.json(projects);
     } catch (error) {
