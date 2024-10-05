04d84d312bd3753b0cfdab31c1b8caacead41590
Edson <EdsonP4100@gmail.com>
2024-10-05 13:46:06 -0700

    Project confirm delete

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -77,4 +77,41 @@ router.post(
 
 router.get('/user', authenticate, AuthController.user);
 
+/* Profile Routes */
+router.put(
+  '/profile',
+  authenticate,
+  body('name').notEmpty().withMessage('El nombre no puede ir vacío'),
+  body('email').isEmail().withMessage('E-mail no válido'),
+  handleInputErrors,
+  AuthController.updateProfile
+);
+
+router.post(
+  '/update-password',
+  authenticate,
+  body('current_password')
+    .notEmpty()
+    .withMessage('El password actual no puede ir vacío'),
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('El password no puede ser menor a 8 caracteres'),
+  body('password_confirmation').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Las contraseñas no coinciden');
+    }
+    return true;
+  }),
+  handleInputErrors,
+  AuthController.updateCurrentUserPassword
+);
+
+router.post(
+  '/check-password',
+  authenticate,
+  body('password').notEmpty().withMessage('El password no puede ir vacío'),
+  handleInputErrors,
+  AuthController.checkPassword
+);
+
 export default router;
